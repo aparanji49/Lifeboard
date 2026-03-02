05e3806e5728e512593f985f8976739eefb6f190
Sai Aparanji Nemmani <nsaiaparanji@gmail.com>
2026-03-02 01:05:02 -0500

    feat: update dependencies and configuration for Prisma, Dexie and authentication

--- lifeboard/next.config.ts
+++ lifeboard/next.config.ts
@@ -8,6 +8,9 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "openweathermap.org",
         pathname: "/img/wn/**",
+      },{
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
       },
     ],
   },
